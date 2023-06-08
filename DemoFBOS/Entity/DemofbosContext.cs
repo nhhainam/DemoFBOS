c46e329d529dfa9a9dddcf35157a1946c3e19849
nhhainam <nhhainam@gmail.com>
2023-06-08 16:00:50 +0700

    change use appsettings

--- DemoFBOS/Entity/DemofbosContext.cs
+++ DemoFBOS/Entity/DemofbosContext.cs
@@ -24,8 +24,16 @@ public partial class DemofbosContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=demofbos;user=root;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+    {
+		if (!optionsBuilder.IsConfigured)
+		{
+			string constr = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().
+								GetConnectionString("MyDb").ToString();
+			optionsBuilder.UseMySql(constr, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+		}
+	}
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseMySql("server=localhost;port=3306;database=demofbos;user=root;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
